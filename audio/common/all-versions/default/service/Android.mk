a3f8428c0ed8943c6f60d700524b6fc0aebfee6a
klozz <klozz@vivaldi.net>
2019-09-23 06:36:48 +0000

    Revert "audio: dont force proprietary com.qualcomm.qti.bluetooth_audio@1.0" we added commit from AOSPA with some files This reverts commit 7a82dc70ea28d56955a33132c8155424f810f6a7.

--- audio/common/all-versions/default/service/Android.mk
+++ audio/common/all-versions/default/service/Android.mk
@@ -53,6 +53,7 @@ LOCAL_SHARED_LIBRARIES := \
     android.hardware.soundtrigger@2.0 \
     android.hardware.soundtrigger@2.1 \
     android.hardware.soundtrigger@2.2 \
+    com.qualcomm.qti.bluetooth_audio@1.0 \
     libhwbinder \
     libcutils
 
